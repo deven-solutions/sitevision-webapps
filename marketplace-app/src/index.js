7dd57cedd0bb1d53065821675ea4200a21be1afe
deven-solutions <deven.solutions@gmail.com>
2021-04-05 17:48:15 +0200

    Upload image in create form

--- marketplace-app/src/index.js
+++ marketplace-app/src/index.js
@@ -38,24 +38,29 @@
   router.post("/create", (req, res) => {
     const currentUser = portletContextUtil.getCurrentUser();
     const userId = currentUser.getIdentifier();
-    const contactInfo = {
-      email: req.params.email,
-      name: req.params.name,
-      phoneNumber: req.params.phoneNumber,
-    };
-    dataStoreProvider.setContactInfo(userId, contactInfo);
     const itemsLimit = appData.get("itemsLimit");
     const userItems = getUserItems();
     if (userItems.length < itemsLimit) {
+      const contactInfo = {
+        email: req.params.email,
+        name: req.params.name,
+        phoneNumber: req.params.phoneNumber,
+      };
+      dataStoreProvider.setContactInfo(userId, contactInfo);
+      const file = req.file('file');
+      const repository = resourceLocatorUtil.getLocalImageRepository();
+      const image = imageUtil.createImageFromTemporary(repository, file);
+      const imageId = image.getIdentifier();
       const item = {
         title: req.params.title,
         description: req.params.description,
         price: req.params.price,
         userId: userId,
         contactInfo: contactInfo,
+        imageId: imageId
       };
-      const id = dataStoreProvider.createItem(item);
-      res.render("/upload", { id: id });
+      dataStoreProvider.createItem(item);
+      renderUserItems(res);
     } else {
       res.render("/itemsLimitExceeded", {});
     }
